20b119757da4a7103c47b6e279e811054255599a
83kjg <83kjg@naver.com>
2022-12-29 14:44:00 +0900

    Update SpringBootApplication.java

--- src/main/java/com/javadeveloperzone/SpringBootApplication.java
+++ src/main/java/com/javadeveloperzone/SpringBootApplication.java
@@ -3,7 +3,6 @@ package com.javadeveloperzone;
 import org.springframework.boot.SpringApplication;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
-
 public class SpringBootApplication {
     public static void main(String[] args){
         SpringApplication.run(SpringBootApplication.class);
